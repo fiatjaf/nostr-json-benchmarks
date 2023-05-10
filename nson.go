8fae5a95c17153b77c788b54196ee8a32d5d1ea0
fiatjaf <fiatjaf@gmail.com>
2023-05-10 08:46:43 -0300

    nson encoder and fixes.

--- nson.go
+++ nson.go
@@ -3,8 +3,8 @@ package benchmarks
 import (
 	"encoding/binary"
 	"encoding/hex"
-	"fmt"
 	"strconv"
+	"strings"
 )
 
 /*
@@ -14,12 +14,12 @@ import (
                    number of tags (let's say it's two)
                      number of items on the first tag (let's say it's three)
                        number of chars on the first item
-                         number of chars on the second item
-                           number of chars on the third item
-                             number of items on the second tag (let's say it's two)
-                               number of chars on the first item
-                                 number of chars on the second item
-   "nson":"xxkkccccttnn112233nn1122"
+                           number of chars on the second item
+                               number of chars on the third item
+                                   number of items on the second tag (let's say it's two)
+                                     number of chars on the first item
+                                         number of chars on the second item
+   "nson":"xxkkccccttnn111122223333nn11112222"
 */
 
 func decodeNson(data string) *Event {
@@ -36,7 +36,6 @@ func decodeNson(data string) *Event {
 	nsonSizeBytes, _ := hex.DecodeString(data[318 : 318+2])
 	nsonSize := int(nsonSizeBytes[0])
 	nsonDescriptors, _ := hex.DecodeString(data[320 : 320+nsonSize])
-	fmt.Println(nsonSizeBytes, nsonSize, nsonDescriptors)
 
 	// dynamic fields
 	kindChars := int(nsonDescriptors[0])
@@ -50,7 +49,6 @@ func decodeNson(data string) *Event {
 	nTags := int(nsonDescriptors[3])
 	evt.Tags = make(Tags, nTags)
 	tagsStart := contentStart + contentChars + 9 // len(`","tags":`)
-	fmt.Println(data[tagsStart-1 : tagsStart+10])
 
 	nsonIndex := 3
 	tagsIndex := tagsStart
@@ -58,13 +56,12 @@ func decodeNson(data string) *Event {
 		nsonIndex++
 		tagsIndex += 1 // len(`[`) or len(`,`)
 		nItems := int(nsonDescriptors[nsonIndex])
-		fmt.Println(t, nItems, tagsIndex, data[tagsIndex:tagsIndex+10])
 		tag := make(Tag, nItems)
 		for n := 0; n < nItems; n++ {
 			nsonIndex++
 			itemStart := tagsIndex + 2 // len(`["`) or len(`,"`)
-			itemChars := int(nsonDescriptors[nsonIndex])
-			fmt.Println(" ", n, itemChars, tagsIndex, data[tagsIndex+2:tagsIndex+2+itemChars+1])
+			itemChars := int(binary.BigEndian.Uint16(nsonDescriptors[nsonIndex:]))
+			nsonIndex++
 			tag[n] = data[itemStart : itemStart+itemChars]
 			tagsIndex = itemStart + itemChars + 1 // len(`"`)
 		}
@@ -75,6 +72,64 @@ func decodeNson(data string) *Event {
 	return &evt
 }
 
-func encodeNson(*Event) []byte {
-	return nil
+func encodeNson(evt *Event) string {
+	// start building the nson descriptors (without the first byte that represents the nson size)
+	nsonBuf := make([]byte, 256)
+
+	// build the tags
+	nTags := len(evt.Tags)
+	nsonBuf[3] = uint8(nTags)
+	nsonIndex := 3 // start here
+
+	tagBuilder := strings.Builder{}
+	tagBuilder.Grow(1000) // a guess
+	tagBuilder.WriteString(`[`)
+	for t, tag := range evt.Tags {
+		nItems := len(tag)
+		nsonIndex++
+		nsonBuf[nsonIndex] = uint8(nItems)
+
+		tagBuilder.WriteString(`[`)
+		for i, item := range tag {
+			v := strconv.Quote(item)
+			nsonIndex++
+			binary.BigEndian.PutUint16(nsonBuf[nsonIndex:], uint16(len(v)-2))
+			nsonIndex++
+			tagBuilder.WriteString(v)
+			if nItems > i+1 {
+				tagBuilder.WriteString(`,`)
+			}
+		}
+		tagBuilder.WriteString(`]`)
+		if nTags > t+1 {
+			tagBuilder.WriteString(`,`)
+		}
+	}
+	tagBuilder.WriteString(`]}`)
+	nsonBuf = nsonBuf[0 : nsonIndex+1]
+
+	kind := strconv.Itoa(evt.Kind)
+	kindChars := len(kind)
+	nsonBuf[0] = uint8(kindChars)
+
+	content := strconv.Quote(evt.Content)
+	contentChars := len(content) - 2
+	binary.BigEndian.PutUint16(nsonBuf[1:3], uint16(contentChars))
+
+	// actually build the json
+	base := strings.Builder{}
+	base.Grow(320 + // everything up to "nson":
+		2 + len(nsonBuf)*2 + // nson
+		9 + kindChars + // kind and its label
+		12 + contentChars + // content and its label
+		9 + tagBuilder.Len() + // tags and its label
+		2, // the end
+	)
+	base.WriteString(`{"id":"` + evt.ID + `","pubkey":"` + evt.PubKey + `","sig":"` + evt.Sig + `","created_at":` + strconv.FormatInt(int64(evt.CreatedAt), 10) + `,"nson":"`)
+	base.WriteString(hex.EncodeToString([]byte{uint8(len(nsonBuf) * 2)})) // nson size
+	base.WriteString(hex.EncodeToString(nsonBuf))                         // nson descriptors
+	base.WriteString(`","kind":` + kind + `,"content":` + content + `,"tags":`)
+	base.WriteString(tagBuilder.String() /* includes the end */)
+
+	return base.String()
 }
